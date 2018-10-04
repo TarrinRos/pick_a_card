b5de1491aec4c5d644b7af087aff3c92ceabda8f
Alexander Smirnov <30797644+TarrinRos@users.noreply.github.com>
2018-10-04 11:07:14 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 
-images = %w(Diamonds Hearts Clubs Spades)
+images = %w(Diamonds Hearts Clubs Spades) # масти
 
 puts "#{values.sample} of #{images.sample}"
