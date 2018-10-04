7294941ac37ca74de9cababc907d4fb04ceb4a16
TarrinRos <30797644+TarrinRos@users.noreply.github.com>
2018-10-04 11:45:40 +0300

    Shows card with unicode symbol

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 
-images = %w(Diamonds Hearts Clubs Spades) # масти
+images = ["\u2660" "\u2663" "\u2665", "\u2666"] # масти в UTF-8
 
-puts "#{values.sample} of #{images.sample}"
+puts "#{values.sample} #{images.sample}"
